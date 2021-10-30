9fc00872926156ba7fa7f4b6b3f9f47db9035bce
Albrecht Striffler <albrecht.striffler@denkbares.com>
2021-10-30 23:41:39 +0200

    Replace File.delete with java.nio.file.Files.delete, because then we will get an exception, if file could not be deleted
    (cherry picked from commit 588426f5da50ea924a4751c6b282df1c05db1f6c)

--- jspwiki-main/src/main/java/org/apache/wiki/providers/VersioningFileProvider.java
+++ jspwiki-main/src/main/java/org/apache/wiki/providers/VersioningFileProvider.java
@@ -550,23 +550,41 @@ public class VersioningFileProvider extends AbstractFileProvider {
     @Override
     public void deletePage( final String page ) throws ProviderException {
         super.deletePage( page );
+        boolean hasError = false;
         final File dir = findOldPageDir( page );
         if( dir.exists() && dir.isDirectory() ) {
             final File[] files = dir.listFiles( new WikiFileFilter() );
             for( final File file : files ) {
-                if (!file.delete()) {
-                    LOG.warn("Failed to delete " + file.getAbsolutePath());
+                try {
+                    Files.delete(file.toPath());
+                }
+                catch (IOException e) {
+                    LOG.error("Can't delete file " + file.getAbsolutePath() + " " + e.getMessage(), e);
+                    hasError = true;
                 }
             }
 
             final File propfile = new File( dir, PROPERTYFILE );
             if( propfile.exists() ) {
-                if (!propfile.delete()) {
-                    LOG.warn("Failed to delete " + propfile.getAbsolutePath());
+                try {
+                    Files.delete(propfile.toPath());
+                }
+                catch (IOException e) {
+                    LOG.error("Can't delete file " + propfile.getAbsolutePath() + " " + e.getMessage(), e);
+                    hasError = true;
                 }
             }
 
-            dir.delete();
+            try {
+                Files.delete(dir.toPath());
+            }
+            catch (IOException e) {
+                LOG.error("Can't delete directory " + propfile.getAbsolutePath() + " " + e.getMessage(), e);
+                hasError = true;
+            }
+            if (hasError) {
+                throw new ProviderException("Can't completely delete the old version for file " + page);
+            }
         }
     }
 
