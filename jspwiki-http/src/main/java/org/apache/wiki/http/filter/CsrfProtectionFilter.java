0681908ab9c752715b794604d1d19a4c58f77d8e
Albrecht Striffler <albrecht.striffler@denkbares.com>
2026-04-01 13:24:23 +0200

    Add possibility to add requests to allowlist for CSRF filtering

--- jspwiki-http/src/main/java/org/apache/wiki/http/filter/CsrfProtectionFilter.java
+++ jspwiki-http/src/main/java/org/apache/wiki/http/filter/CsrfProtectionFilter.java
@@ -32,6 +32,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.wiki.api.core.Engine;
 import org.apache.wiki.api.core.Session;
 import org.apache.wiki.api.spi.Wiki;
+import org.apache.wiki.util.CsrfProtectionAllowList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -58,7 +59,7 @@ public class CsrfProtectionFilter implements Filter {
 	 */
 	@Override
 	public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
-		if (isPost((HttpServletRequest) request)) {
+		if (isPost((HttpServletRequest) request) && !CsrfProtectionAllowList.isAllowListed((HttpServletRequest) request)) {
 			final Engine engine = Wiki.engine().find(request.getServletContext(), null);
 			final Session session = Wiki.session().find(engine, (HttpServletRequest) request);
 			if (!requestContainsValidCsrfToken(request, session)) {
@@ -74,6 +75,9 @@ public class CsrfProtectionFilter implements Filter {
 
 	public static boolean isCsrfProtectedPost(final HttpServletRequest request) {
 		if (isPost(request)) {
+			if (CsrfProtectionAllowList.isAllowListed(request)) {
+				return true;
+			}
 			final Engine engine = Wiki.engine().find(request.getServletContext(), null);
 			final Session session = Wiki.session().find(engine, request);
 			return requestContainsValidCsrfToken(request, session);
