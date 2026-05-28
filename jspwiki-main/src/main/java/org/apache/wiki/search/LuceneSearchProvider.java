15cfd808e89b5b2a35fc47f2ae3b916e59b7e663
Albrecht Striffler <albrecht.striffler@denkbares.com>
2026-05-28 15:43:25 +0200

    planned step 7.2.18: test custom analyzers/filters/writers and fix tests

--- jspwiki-main/src/main/java/org/apache/wiki/search/LuceneSearchProvider.java
+++ jspwiki-main/src/main/java/org/apache/wiki/search/LuceneSearchProvider.java
@@ -34,7 +34,7 @@ import java.util.Properties;
 import java.util.concurrent.Executor;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.lucene.analysis.standard.ClassicAnalyzer;
+import org.apache.lucene.analysis.classic.ClassicAnalyzer;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.apache.lucene.analysis.Analyzer;
